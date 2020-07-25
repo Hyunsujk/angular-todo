f8f8e1fd9dc8264d003559c95100f145114af85c
Hyunsujk <hyunsujk@gmail.com>
2020-07-25 14:29:58 -0500

    added title

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   templateUrl: './input-button-unit.component.html',
-  styleUrls: ['./input-button-unit.component.scss']
+  styleUrls: ['./input-button-unit.component.scss'],
 })
 export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
