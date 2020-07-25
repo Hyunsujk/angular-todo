41415d9dc19f5fd2e0b3d8e4eb7c9a487038478b
Hyunsujk <hyunsujk@gmail.com>
2020-07-25 14:11:11 -0500

    added inline template example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: ` <h1>Welcome to {{ title }}!</h1> `, // inline - template
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
