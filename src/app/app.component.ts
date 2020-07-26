6ccb0884e89167d41e72a5cc4c9044f37b0c11cd
Hyunsujk <hyunsujk@gmail.com>
2020-07-26 18:55:15 -0500

    added comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,6 @@ export class AppComponent {
   ];
 
   addItem(title: string) {
-    this.todoList.push({ title });
+    this.todoList.push({ title }); // same with {title:title} ES6 Object Property Value Shorthand
   }
 }
