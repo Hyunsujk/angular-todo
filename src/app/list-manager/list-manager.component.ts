2406e91844939567efd85d637f9c8eda1760e5ed
Hyunsujk <hyunsujk@gmail.com>
2020-07-26 19:25:19 -0500

    refactored

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-list-manager',
   templateUrl: './list-manager.component.html',
-  styleUrls: ['./list-manager.component.scss']
+  styleUrls: ['./list-manager.component.scss'],
 })
 export class ListManagerComponent implements OnInit {
+  todoList: TodoItem[] = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  addItem(title: string) {
+    this.todoList.push({ title }); // same with {title:title} ES6 Object Property Value Shorthand
   }
-
 }
