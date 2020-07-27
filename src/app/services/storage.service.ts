476b5bc6551e3324028d82af5275df4eda80d16b
Hyunsujk <hyunsujk@gmail.com>
2020-07-26 20:52:03 -0500

    created getter and setter

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
+  constructor() {}
 
-  constructor() { }
+  getData(key: string): any {
+    // parse the string into a valid JavaScript object
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  setData(key: string, data: any) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
 }
